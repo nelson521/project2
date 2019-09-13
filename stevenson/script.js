1672b3b2b06c831488058c5be8515a96df3a708d
Stevenson Nelson <sonson521@gmail.com>
2019-09-12 20:53:34 -0400

    added geo code

--- stevenson/script.js
+++ stevenson/script.js
@@ -1,5 +1,24 @@
 const apiKey = '6a9563de61b6bf8d11bf1447109c610d';
-const queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=39.9524&lon=-75.1636&units=imperial&APPID=' + apiKey;
+let lat = 39.9524;
+let lon = -75.1636;
+const queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ lon +'&units=imperial&APPID=' + apiKey;
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+
+  lat = position.coords.lat;
+  lon = position.coords.lon;
+  //  console.log(position.coords.latitude);
+  //  console.log(position.coords.longitude);
+  //  console.log(position.coords);
+
+}
+
+getLocation();
+
 $(document).ready(function () {
   
   $.ajax({
@@ -9,5 +28,6 @@ $(document).ready(function () {
     $('#apiName').text(JSON.stringify(response.name).replace(/\"/g, ""));
     $('#apiWeather').text(JSON.stringify(response.main.temp))
     console.log(response)
+
   });
 });
